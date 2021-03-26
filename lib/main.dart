5795f12d652a2d4ca1f1602e3f8590cdf47dad7e
AKoszewski <akoszews@gmail.com>
2021-03-26 19:00:55 +0100

    Web view communication

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,69 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
+      title: 'Communication Bridge',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Native - JS Communication Bridge'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  WebViewController _controller;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Webview')),
+      body: WebView(
+        initialUrl: 'about:blank',
+        javascriptMode: JavascriptMode.unrestricted,
+        javascriptChannels: Set.from([
+          JavascriptChannel(
+              name: 'messageHandler',
+              onMessageReceived: (JavascriptMessage message) {
+                print(message.message);
+              })
+        ]),
+        onWebViewCreated: (WebViewController webviewController) {
+          _controller = webviewController;
+          _loadHtmlFromAssets();
+        },
       ),
     );
   }
+
+  _loadHtmlFromAssets() async {
+    String file = await rootBundle.loadString('assets/index.html');
+    _controller.loadUrl(Uri.dataFromString(
+        file,
+        mimeType: 'text/html',
+        encoding: Encoding.getByName('utf-8')).toString());
+  }
+
 }
