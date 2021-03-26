bd748005c125033d0dc3b7fbdd9fe89fac395f17
AKoszewski <akoszews@gmail.com>
2021-03-27 00:32:55 +0100

    Zapisywanie loginu i hasla teraz w pelni dziala

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,28 @@ class User {
   final String pass;
   User({this.id, this.login, this.pass});
   Map<String, dynamic> toMap() {
-    return 
-    {
+    return {
       'id': id,
       'login': login,
       'pass': pass,
     };
   }
+
   @override
   String toString() {
     return 'User{id: $id, login: $login, pass: $pass}';
   }
 }
 
+Future<Database> database;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  database = openDatabase(join(await getDatabasesPath(), 'user.db'),
+      onCreate: (db, version) {
+    return db.execute(
+        "CREATE TABLE users(id INTEGER PRIMARY KEY, login TEXT, pass TEXT)");
+  }, version: 1);
   runApp(MyApp());
 }
 
@@ -78,7 +85,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   WebViewController _controller;
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   String loginUrl = 'https://dziennikhodowlany.pl/admin/users/login';
@@ -88,68 +94,67 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: WebView(
-        initialUrl: loginUrl,
-        javascriptMode: JavascriptMode.unrestricted,
-        javascriptChannels: Set.from([
-          JavascriptChannel(
-              name: 'messageHandler',
-              onMessageReceived: (JavascriptMessage message) async {
-                final Future<Database> database = openDatabase(join(await getDatabasesPath(), 'user.db'),
-                  onCreate: (db, version) {
-                    return db.execute("CREATE TABLE users(id INTEGER PRIMARY KEY, login TEXT, pass TEXT)");
-                  },
-                  version: 1);
-                Future<void> insertUser(User user) async {
-                  final Database db = await database;
-                  await db.insert(
-                    'users',
-                    user.toMap(),
-                    conflictAlgorithm: ConflictAlgorithm.replace,
-                  );
-                }
-                Future<List<User>> users() async {
-                  final Database db = await database;
-                  final List<Map<String, dynamic>> maps = await db.query('users');
-                  return List.generate(maps.length, (i) {
-                    return User(
-                      id: maps[i]['id'],
-                      login: maps[i]['login'],
-                      pass: maps[i]['pass'],
+          initialUrl: loginUrl,
+          javascriptMode: JavascriptMode.unrestricted,
+          javascriptChannels: Set.from([
+            JavascriptChannel(
+                name: 'messageHandler',
+                onMessageReceived: (JavascriptMessage message) async {
+                  Future<void> insertUser(User user) async {
+                    final Database db = await database;
+                    await db.insert(
+                      'users',
+                      user.toMap(),
+                      conflictAlgorithm: ConflictAlgorithm.replace,
                     );
-                  });
-                }
-                print(message.message);
-                var credentials = message.message.split(";");
-                await insertUser(User(id: 0, login: credentials[0], pass: credentials[1]));
-                print(await users());
-              })
-        ]),
-        onWebViewCreated: (WebViewController webviewController) {
-          _controller = webviewController;
-          //_loadHtmlFromAssets();
-        },
-        onPageFinished: (String url) async {
-          if (url.contains(loginUrl)) {
-            String login = "akoszews@gmail.com";
-            String pass = "Haslo123";
-            _controller.evaluateJavascript('document.getElementById("username").value = "$login"');
-            _controller.evaluateJavascript('document.getElementById("password").value = "$pass"');
-            _controller.evaluateJavascript('''document.getElementsByClassName("buttonlogin")[0].onclick =
+                  }
+
+                  print(message.message);
+                  var credentials = message.message.split(";");
+                  await insertUser(
+                      User(id: 0, login: credentials[0], pass: credentials[1]));
+                })
+          ]),
+          onWebViewCreated: (WebViewController webviewController) {
+            _controller = webviewController;
+            //_loadHtmlFromAssets();
+          },
+          onPageFinished: (String url) async {
+            if (url == loginUrl) {
+              Future<List<User>> users() async {
+                final Database db = await database;
+                final List<Map<String, dynamic>> maps = await db.query('users');
+                return List.generate(maps.length, (i) {
+                  return User(
+                    id: maps[i]['id'],
+                    login: maps[i]['login'],
+                    pass: maps[i]['pass'],
+                  );
+                });
+              }
+              var userList = await users();
+              var cred = userList[0];
+              String login = cred.login;
+              String pass = cred.pass;
+              _controller.evaluateJavascript(
+                  'document.getElementById("username").value = "$login"');
+              _controller.evaluateJavascript(
+                  'document.getElementById("password").value = "$pass"');
+              _controller.evaluateJavascript(
+                  '''document.getElementsByClassName("buttonlogin")[0].onclick =
                                                 function() {
                                                   var login = document.getElementById("username").value
                                                   var pass = document.getElementById("password").value
                                                   messageHandler.postMessage(login+";"+pass);
                                                 }
                                                 ''');
-          }
-          else if (url.contains(indexUrl)) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Menu()),
-            );
-          }
-        }
-      ),
+            } else if (url.contains(indexUrl)) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Menu()),
+              );
+            }
+          }),
     );
   }
 }
