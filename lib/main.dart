250ffa76a45fb00f257a82a2e65dbfbbc6e50948
AKoszewski <akoszews@gmail.com>
2021-03-31 23:28:07 +0200

    Przepisany layout

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ import 'package:sqflite/sqflite.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart' hide JavascriptChannel, JavascriptMessage;
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart'
+    hide JavascriptChannel, JavascriptMessage;
 
 // Color bgColor = Color(0xFF373C42);
 Color bgColor = Color(0xFF353842);
@@ -33,6 +34,7 @@ double getFontSize(BuildContext context) {
 
 double sideMargin = 20;
 double statusBarHeight = 24;
+double spacing = 20;
 
 class User {
   final int id;
@@ -82,9 +84,8 @@ class MyApp extends StatelessWidget {
 }
 
 class AppButton extends StatelessWidget {
-  AppButton({this.label, this.color, this.top, this.imgPath, @required this.onPressed});
+  AppButton({this.label, this.color, this.imgPath, @required this.onPressed});
   final GestureTapCallback onPressed;
-  final double top;
   final String imgPath;
   final String label;
   final Color color;
@@ -92,29 +93,27 @@ class AppButton extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double buttonHeight = getButtonHeight(context);
-    double btnMargin = 20;
 
-    return Positioned(
-      left: btnMargin,
-      top: this.top,
-      child: ConstrainedBox(
-        constraints: BoxConstraints.tightFor(
-            width: screenWidth - 2 * btnMargin, height: buttonHeight),
-        child: TextButton(
-          onPressed: this.onPressed,
-          // icon: Image.asset(this.imgPath,
-          //     height: 20, width: 20, color: Colors.white),
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(this.color),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                //side: BorderSide(color: Colors.red),
-              ),
+    return Container(
+      width: double.infinity,
+      height: buttonHeight,
+      child: TextButton(
+        onPressed: this.onPressed,
+        // icon: Image.asset(this.imgPath,
+        //     height: 20, width: 20, color: Colors.white),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(this.color),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
             ),
           ),
-          child: Text(this.label, style: TextStyle(fontSize: getFontSize(context), color: Colors.white,)),
         ),
+        child: Text(this.label,
+            style: TextStyle(
+              fontSize: getFontSize(context),
+              color: Colors.white,
+            )),
       ),
     );
   }
@@ -142,102 +141,127 @@ class Menu extends StatelessWidget {
   Widget build(BuildContext context) {
     double buttonHeight = getButtonHeight(context);
     return WillPopScope(
-      onWillPop: () async {
-        return;
-      },
-      child: Scaffold(
-        backgroundColor: bgColor,
-        body: Center(
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                top: 0.125*buttonHeight + statusBarHeight,
-                left: sideMargin,
-                child:Row(
+        onWillPop: () async {
+          return;
+        },
+        child: Scaffold(
+          backgroundColor: bgColor,
+          body: Container(
+            padding: EdgeInsets.only(
+                left: 20.0, right: 20.0, top: statusBarHeight, bottom: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
                   children: <Widget>[
-                    SizedBox(width: 0.25*buttonHeight, height: 0.25*buttonHeight),
-                    Image(image: AssetImage('./assets/icon/icon.png'), height: 0.75*buttonHeight, width: 0.75*buttonHeight),
-                    SizedBox(width: 0.125*buttonHeight, height: 0.125*buttonHeight),
-                    Text("Dziennik hodowlany", style: TextStyle(color: Colors.white, fontSize: 1.2 * getFontSize(context))),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 0.125 * buttonHeight),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          SizedBox(
+                              width: 0.25 * buttonHeight,
+                              height: 0.25 * buttonHeight),
+                          Image(
+                              image: AssetImage('./assets/icon/icon.png'),
+                              height: 0.75 * buttonHeight,
+                              width: 0.75 * buttonHeight),
+                          SizedBox(
+                              width: 0.125 * buttonHeight,
+                              height: 0.125 * buttonHeight),
+                          Text("Dziennik hodowlany",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 1.2 * getFontSize(context))),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 1.0,
+                      width: double.infinity,
+                      color: buttonColor,
+                    ),
+                    AppButton(
+                      label: "Moje konto",
+                      imgPath: "./assets/img/icon1.png",
+                      color: bgColor,
+                      onPressed: () async {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Index(indexUrl)),
+                        );
+                      },
+                    ),
+                    Container(
+                      height: 1.0,
+                      width: double.infinity,
+                      color: buttonColor,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 0.475 * buttonHeight,
+                    ),
+                    Row(children: <Widget>[
+                      SizedBox(width: 10, height: 10),
+                      Text("skanuj QR & dodaj wpis",
+                          style: TextStyle(
+                            fontSize: getFontSize(context),
+                            color: Colors.white,
+                          )),
+                    ]),
+                    SizedBox(width: double.infinity, height: spacing),
+                    AppButton(
+                      label: "Pomiary",
+                      imgPath: "./assets/img/icon2.png",
+                      color: buttonColor,
+                      onPressed: () async {
+                        await scanQr(context, "measurements");
+                      },
+                    ),
+                    SizedBox(width: double.infinity, height: spacing),
+                    AppButton(
+                      label: "Wylinki",
+                      imgPath: "./assets/img/icon3.png",
+                      color: buttonColor,
+                      onPressed: () async {
+                        await scanQr(context, "old-skins");
+                      },
+                    ),
+                    SizedBox(width: double.infinity, height: spacing),
+                    AppButton(
+                      label: "Karmienie",
+                      imgPath: "./assets/img/icon4.png",
+                      color: buttonColor,
+                      onPressed: () async {
+                        await scanQr(context, "feedings");
+                      },
+                    ),
                   ],
                 ),
-              ),
-              Positioned(
-                top: 1.125*buttonHeight + statusBarHeight,
-                left: sideMargin,
-                child: Container(
-                  height: 1.0,
-                  width: getScreenWidth(context) - 2 * sideMargin,
-                  color: buttonColor,
+                Column(
+                  children: <Widget>[
+                    Container(
+                      height: 1.0,
+                      margin: EdgeInsets.only(bottom: 3),
+                      width: double.infinity,
+                      color: buttonColor,
+                    ),
+                    AppButton(
+                      label: "Wyloguj",
+                      imgPath: "./assets/img/icon5.png",
+                      color: logoutButtonColor,
+                      onPressed: () async {
+                        Phoenix.rebirth(context);
+                      },
+                    ),
+                  ],
                 ),
-              ),
-              AppButton(
-                top: 1.125*buttonHeight + statusBarHeight + 1,
-                label: "Moje konto",
-                imgPath: "./assets/img/icon1.png",
-                color: bgColor,
-                onPressed: () async {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Index(indexUrl)),
-                  );
-                },
-              ),
-              Positioned(
-                top: 2.125*buttonHeight + statusBarHeight + 1,
-                left: sideMargin,
-                child: Container(
-                  height: 1.0,
-                  width: getScreenWidth(context) - 2 * sideMargin,
-                  color: buttonColor,
-                ),
-              ),
-              Positioned (
-                top: 2.6*buttonHeight + statusBarHeight + 1,
-                left: 30,
-                child: Text("skanuj QR & dodaj wpis", style: TextStyle(fontSize: getFontSize(context), color: Colors.white,)),
-              ),
-              AppButton(
-                top: 3.125*buttonHeight + statusBarHeight + 1,
-                label: "Pomiary",
-                imgPath: "./assets/img/icon2.png",
-                color: buttonColor,
-                onPressed: () async {
-                  await scanQr(context, "measurements");
-                },
-              ),
-              AppButton(
-                top: 4.125*buttonHeight + statusBarHeight + 21,
-                label: "Wylinki",
-                imgPath: "./assets/img/icon3.png",
-                color: buttonColor,
-                onPressed: () async {
-                  await scanQr(context, "old-skins");
-                },
-              ),
-              AppButton(
-                top: 5.125*buttonHeight + statusBarHeight + 41,
-                label: "Karmienie",
-                imgPath: "./assets/img/icon4.png",
-                color: buttonColor,
-                onPressed: () async {
-                  await scanQr(context, "feedings");
-                },
-              ),
-              AppButton(
-                top: 0.864*getScreenHeight(context),
-                label: "Wyloguj",
-                imgPath: "./assets/img/icon5.png",
-                color: logoutButtonColor,
-                onPressed: () async {
-                  Phoenix.rebirth(context);
-                },
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
 
@@ -249,9 +273,8 @@ class Index extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: WebviewScaffold(
-        url: url,
-      )
-    );
+      url: url,
+    ));
   }
 }
 
@@ -297,7 +320,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ]),
           onWebViewCreated: (WebViewController webviewController) {
             _controller = webviewController;
-            //_loadHtmlFromAssets();
           },
           onPageFinished: (String url) async {
             if (url == loginUrl) {
